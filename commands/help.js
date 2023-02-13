ef1de3e52c209105547922758bab8be58c9908b1
ASHA-XD <125123443+ASHA-XD@users.noreply.github.com>
2023-02-13 13:15:40 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -39,9 +39,9 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${
+  }) | [GitHub](https://github.com/ASHA-XD/Discord-MusicBot) | [Dashboard](${
       client.botconfig.Website
-    }) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+    }) | By [ASHA-XD](https://github.com/ASHA-XD)`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
@@ -125,9 +125,9 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${
+  }) | [GitHub](https://github.com/ASHA-XD/Discord-MusicBot) | [Dashboard](${
         client.botconfig.Website
-      }) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+      }) | By [ASHA-XD](https://github.com/ASHA-XD)`);
       if (!args) return interaction.send(Embed);
       else {
         let cmd =
